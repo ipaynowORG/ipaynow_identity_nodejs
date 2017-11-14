6f18681072b2de666240114055362017e2824179
张江南 <zhangjiangnan@ipaynow.cn>
2017-11-14 19:25:06 +0800

    1

---
+++ demo.js
@@ -0,0 +1,22 @@
+
+
+//SDK依赖
+var sdk = require('./sdk');
+//AppId和AppKey
+// var app = new sdk.App("1441071499740581","DK96gnOB7EmVDDaHgLTLEZqVgP0H0nML","abnMX6YXHjBdesCsn2TD8b25")
+// sdk.IdentityAuthQuery(app,"1122334")
+
+// var app = new sdk.App("1441071499740581","DK96gnOB7EmVDDaHgLTLEZqVgP0H0nML","abnMX6YXHjBdesCsn2TD8b25")
+// sdk.IdentityAuth(app,"张江南","110101198204031532","")
+
+// var app = new sdk.App("1441071499740581","DK96gnOB7EmVDDaHgLTLEZqVgP0H0nML","Z5CbXbEaJA6CYRSDS5Ddsab6")
+// sdk.CardAuth(app,"大王","230128199207080925","6226200105111414","")
+
+// var app = new sdk.App("1441071499740581","DK96gnOB7EmVDDaHgLTLEZqVgP0H0nML","Z5CbXbEaJA6CYRSDS5Ddsab6")
+// sdk.CardAuthQuery(app,"7ETHZX29LEBHIZH69WFJ")
+
+// var app = new sdk.App("1476788813528909","c4MlX9GJCi0YI5z3RvpK17wPlscFKpY1","dYTAwfNMEeT3pk5FB8MS5MYk")
+// sdk.MobileNoAuth(app,"张江南","110101198204031532","13401190417","")
+
+// var app = new sdk.App("1476788813528909","c4MlX9GJCi0YI5z3RvpK17wPlscFKpY1","dYTAwfNMEeT3pk5FB8MS5MYk")
+// sdk.MobileNoAuthQuery(app,"PWRGPQ63NDI1S8T66CX2")
---
+++ sdk.js
@@ -0,0 +1,235 @@
+
+
+var util = require('./util');
+
+exports.App = function (appId,appKey,desKey){
+	this.appId=appId;
+	this.appKey=appKey;
+	this.desKey = desKey;
+}
+
+     /**
+     * 身份验证
+     * @param app appId(应用ID)和appKey ,desKey
+     * 登录商户后台 : https://mch.ipaynow.cn ->商户中心->应用信息可以新增应用或查看appKey
+     * @param cardName  姓名
+     * @param idcard    身份证
+     * @param mhtOrderNo    商户订单号(可空,为空时自动生成)
+     * @return
+     */
+exports.IdentityAuth = function (app,cardName,idcard,mhtOrderNo){
+	var m = {};
+	
+	m['cardName'] = cardName;
+	m['idcard'] = idcard;
+	if(mhtOrderNo != ""){
+		m['mhtOrderNo'] = mhtOrderNo;
+	}else{
+		
+		m['mhtOrderNo'] = util.generateMixed(20);
+	}
+	return query(app,m,"ID01")
+}
+
+
+	/**
+     * 身份验证-订单查询
+     * @param app appId(应用ID)和appKey ,desKey
+     * 登录商户后台 : https://mch.ipaynow.cn ->商户中心->应用信息可以新增应用或查看appKey
+     * @param mhtOrderNo    商户订单号
+     * @return
+     */
+exports.IdentityAuthQuery = function (app,mhtOrderNo){
+	var m = {};
+
+	m['mhtOrderNo'] = mhtOrderNo;
+	return query(app,m,"ID01_Query")
+}
+
+
+
+
+	/**
+     * 卡信息认证
+     * @param app appId(应用ID)和appKey ,desKey
+     * 登录商户后台 : https://mch.ipaynow.cn ->商户中心->应用信息可以新增应用或查看appKey
+     * @param idCardName   姓名
+     * @param idCard    身份证
+     * @param bankCardNum   银行账户
+     * @param mhtOrderNo    商户订单号(可空,为空时自动生成)
+     * @return
+     */
+exports.CardAuth = function (app,idCardName,idCard,bankCardNum,mhtOrderNo){
+	var m = {};
+	
+	m['idCardName'] = idCardName;
+	m['idCard'] = idCard;
+	m['bankCardNum'] = bankCardNum;
+	if(mhtOrderNo != ""){
+		m['mhtOrderNo'] = mhtOrderNo;
+	}else{
+		
+		m['mhtOrderNo'] = util.generateMixed(20);
+	}
+	return query(app,m,"ID02")
+}
+
+
+
+
+
+
+	/**
+     * 卡信息认证- 订单查询
+     * @param app appId(应用ID)和appKey ,desKey
+     * 登录商户后台 : https://mch.ipaynow.cn ->商户中心->应用信息可以新增应用或查看appKey
+     * @param mhtOrderNo
+     * @return
+     */
+exports.CardAuthQuery = function (app,mhtOrderNo){
+	var m = {};
+	
+	m['mhtOrderNo'] = mhtOrderNo;
+	return query(app,m,"ID02_Query")
+}
+
+
+
+
+
+
+
+
+ 	/**
+     * 手机号认证
+     * @param app appId(应用ID)和appKey ,desKey
+     * 登录商户后台 : https://mch.ipaynow.cn ->商户中心->应用信息可以新增应用或查看appKey
+     * @param idCardName    认证姓名
+     * @param idCard    身份证号码
+     * @param mobile    手机号
+     * @param mhtOrderNo    商户订单号
+     * @return
+     */
+exports.MobileNoAuth = function (app,idCardName,idCard,mobile,mhtOrderNo){
+	var m = {};
+	
+	m['idCardName'] = idCardName;
+	m['idCard'] = idCard;
+	m['mobile'] = mobile;
+	if(mhtOrderNo != ""){
+		m['mhtOrderNo'] = mhtOrderNo;
+	}else{
+		
+		m['mhtOrderNo'] = util.generateMixed(20);
+	}
+	return query(app,m,"ID03")
+}
+
+
+
+
+
+
+	/**
+     * 手机号认证 - 订单查询
+     * @param app appId(应用ID)和appKey ,desKey
+     * 登录商户后台 : https://mch.ipaynow.cn ->商户中心->应用信息可以新增应用或查看appKey
+     * @param mhtOrderNo
+     * @return
+     */
+exports.MobileNoAuthQuery = function (app,mhtOrderNo){
+	var m = {};
+	
+	m['mhtOrderNo'] = mhtOrderNo;
+	return query(app,m,"ID03_Query")
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function query(app,requestMap,funcode) {
+
+	content = util.postFormLinkReport(requestMap);
+
+	var message1 = "appId="+app.appId+"";
+	message1 = new Buffer(message1).toString('base64');
+
+	
+
+	var message2 = content;
+	message2 = util.encrypt(message2,app.desKey);
+
+	
+
+	var message3 = new Buffer(util.md5(content+"&"+app.appKey)).toString('base64');	
+
+	
+
+	var message = message1+"|"+message2+"|"+message3+"";
+	
+	message = encodeURIComponent(message);
+
+	
+
+	return post("funcode="+funcode+"&message="+message,"https://dby.ipaynow.cn/identify",app);
+}
+
+
+
+
+
+
+
+
+
+
+function post(content,posturl,app){
+	var https = require('https');
+	var util = require('util');
+	var url = require('url');
+	var querystring = require('querystring');
+
+
+	var post_option = url.parse(posturl);
+	post_option.method = "POST";
+
+	post_option.headers = {
+    	'Content-Type' : 'text/html',
+    	'Content-Length' : content.length
+	};
+	
+	var result = "";
+	var post_req = https.request(post_option,function(res){
+    	res.setEncoding('utf8');
+    	res.on('data',function(chunk){
+        	result += chunk;
+    	});
+    	res.on('end', function() {
+            if(result.split("|").length==2){	
+			}else{
+            	var return2 = result.split("|")[1];
+
+				var util = require('./util');
+            	var originalMsg = util.decrypt(return2.replace(/[\r\n]/g,""),app.desKey);
+				console.log(originalMsg);
+			}
+
+        });
+    
+	});
+	post_req.write(content);
+	post_req.end();
+	return result;
+}
